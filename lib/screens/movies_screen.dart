48128040e34b3be52b824d3017667eb08b26153f
MuhammadNoor <noor4apps@gmail.com>
2022-04-26 02:16:59 +0300

    movies screen

--- lib/screens/movies_screen.dart
+++ lib/screens/movies_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:movies_app/controllers/movie_controller.dart';
+import 'package:movies_app/models/movie.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MoviesScreen extends StatefulWidget {
   late final String? type;
@@ -29,11 +31,77 @@ class _MoviesScreenState extends State<MoviesScreen> {
       appBar: AppBar(
         title: Text('Movies', style: TextStyle(fontSize: 20, color: Colors.green, fontWeight: FontWeight.w600)),
       ),
-      body: moviesController.isLoading.value
-          ? Center(child: CircularProgressIndicator())
-          : Container(
-              child: null,
-            ),
+        body: Obx(() {
+          return moviesController.isLoading.value == true
+              ? Container(
+                  child: Center(child: CircularProgressIndicator())
+                )
+              : Container(
+                  padding: const EdgeInsets.all(12),
+                  child: SingleChildScrollView(
+                    child: ListView.separated(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return buildMovieItem(moviesController.movies.value[index]);
+                      },
+                      separatorBuilder: (context, index) {
+                        return SizedBox(height: 10);
+                      },
+                      itemCount: moviesController.movies.length,
+                    ),
+                  ),
+                );
+        }));
+  }
+
+  Widget buildMovieItem(Movie movie) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          width: 130,
+          height: 200,
+          child: Stack(
+            children: [
+              Center(child: CircularProgressIndicator()),
+              FadeInImage.memoryNetwork(
+                image: '${movie.poster}',
+                placeholder: kTransparentImage,
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 15),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Text('${movie.title}', style: TextStyle(fontSize: 18, color: Colors.green, fontWeight: FontWeight.w600), overflow: TextOverflow.ellipsis, maxLines: 2),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.star, color: Colors.yellow),
+                      Text('${movie.vote}', style: TextStyle(fontSize: 18, color: Colors.lightGreen))
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                '${movie.description}',
+                style: TextStyle(fontSize: 15, color: Colors.lightGreen[200]),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 8,
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 
